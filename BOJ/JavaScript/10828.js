cd21fa0d44ad771e58d0e4bcd3f84fa886ea5167
leo-xee <jang.xmin@gmail.com>
2024-06-18 00:44:14 +0900

    Upload 10828 - 스택
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

--- BOJ/JavaScript/10828.js
+++ BOJ/JavaScript/10828.js
@@ -1,3 +1,49 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().split("\n");
+
+const solution = (input) => {
+  const arr = [];
+  const res = [];
+
+  for (let v of input) {
+    const [cmd, value] = v.split(" ");
+
+    if (cmd === "push") {
+      arr.push(+value);
+    }
+
+    if (cmd === "pop") {
+      if (arr.length === 0) {
+        res.push(-1);
+      } else {
+        res.push(arr.pop());
+      }
+    }
+
+    if (cmd === "size") {
+      res.push(arr.length);
+    }
+
+    if (cmd === "empty") {
+      if (arr.length === 0) {
+        res.push(1);
+      } else {
+        res.push(0);
+      }
+    }
+
+    if (cmd === "top") {
+      if (arr.length === 0) {
+        res.push(-1);
+      } else {
+        res.push(arr[arr.length - 1]);
+      }
+    }
+  }
+
+  return res.join("\n");
+};
+
+input.shift();
+console.log(solution(input));
