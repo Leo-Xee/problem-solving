205f0917c1b535a633845ab1438007dce1257e33
leo-xee <jang.xmin@gmail.com>
2024-06-19 00:37:55 +0900

    Upload 9093 - 단어 뒤집기
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ BOJ/JavaScript/9093.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+input.shift();
+
+const solution = (input) => {
+  const result = [];
+
+  for (const sentence of input) {
+    const words = sentence.split(" ");
+
+    for (const word of words) {
+      const stack = [];
+
+      for (const letter of word) {
+        stack.push(letter);
+      }
+
+      for (_ of word) {
+        result.push(stack.pop());
+      }
+
+      result.push(" ");
+    }
+    result.push("\n");
+  }
+
+  return result.join("");
+};
+
+console.log(solution(input));
