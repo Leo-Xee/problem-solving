d626374ede03e9a59692a5b29e01d6ca8ce115d3
leo-xee <jang.xmin@gmail.com>
2024-07-24 22:47:10 +0900

    Upload 10808 - 알파벳 개수
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ BOJ/C++/10808.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int a[26];
+int main() {
+  cin >> s;
+
+  for (int i = 0; i < s.size(); i++) {
+    a[s[i] - 97]++;
+  }
+
+  for (int i : a) cout << i << " ";
+
+  return 0;
+}
