8a468b769b1e4ca8bd68731be95eee2b7ff96d8a
leo-xee <jang.xmin@gmail.com>
2024-08-11 16:45:45 +0900

    Upload 1940 - 주몽(재귀)
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

--- BOJ/C++/1940.cpp
+++ BOJ/C++/1940.cpp
@@ -1,25 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, cnt;
-int a[15004];
+// int n, m, cnt;
+// int a[15004];
+// int main() {
+//   ios_base::sync_with_stdio(false);
+//   cin.tie(NULL);
+
+//   cin >> n >> m;
+
+//   for (int i = 0; i <= n; i++) {
+//     cin >> a[i];
+//   }
+
+//   for (int i = 0; i < n; i++) {
+//     for (int j = i + 1; j < n; j++) {
+//       if (a[i] + a[j] == m) cnt++;
+//     }
+//   }
+
+//   cout << cnt << '\n';
+
+//   return 0;
+// }
+
+// 재귀 활용
+int n, m, a[15004], cnt;
+
+void combi(int start, vector<int>& v) {
+  if (v.size() == 2) {
+    int b = a[v[0]];
+    int c = a[v[1]];
+    if (b + c == m) cnt++;
+    return;
+  }
+
+  for (int i = start + 1; i < n; i++) {
+    v.push_back(i);
+    combi(i, v);
+    v.pop_back();
+  }
+}
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
   cin >> n >> m;
-
-  for (int i = 0; i <= n; i++) {
+  for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
 
-  for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if (a[i] + a[j] == m) cnt++;
-    }
-  }
-
+  vector<int> v;
+  combi(-1, v);
   cout << cnt << '\n';
-
   return 0;
 }
