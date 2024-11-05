48c2ee1a0a2ece50125a7e3b83791cae00909d1d
leo-xee <jang.xmin@gmail.com>
2024-11-06 00:52:51 +0900

    Upload 2468 - 안전 영역
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ BOJ/C++/2468.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int n, a[101][101], visited[101][101], ret = 1;
+
+void dfs(int y, int x, int d) {
+  visited[y][x] = 1;
+
+  for (int i = 0; i < 4; i++) {
+    int ny = y + dy[i];
+    int nx = x + dx[i];
+
+    if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+    if (visited[ny][nx] || a[ny][nx] <= d) continue;
+    dfs(ny, nx, d);
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> a[i][j];
+    }
+  }
+
+  for (int d = 1; d < 101; d++) {
+    fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (a[i][j] > d && !visited[i][j]) {
+          dfs(i, j, d);
+          cnt++;
+        }
+      }
+    }
+
+    ret = max(ret, cnt);
+  }
+
+  cout << ret << '\n';
+
+  return 0;
+}
---
+++ BOJ/JavaScript/2468.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let n = +input.shift();
+let a = input.map((v) => v.split(" ").map((v) => +v));
+let visited = [...Array(n)].map(() => Array(n).fill(0));
+
+let ret = 1;
+let dy = [-1, 0, 1, 0];
+let dx = [0, 1, 0, -1];
+
+const dfs = (y, x, d) => {
+  visited[y][x] = 1;
+
+  for (let i = 0; i < 4; i++) {
+    let ny = y + dy[i];
+    let nx = x + dx[i];
+
+    if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+    if (visited[ny][nx] || a[ny][nx] <= d) continue;
+    dfs(ny, nx, d);
+  }
+};
+
+for (let d = 1; d < 101; d++) {
+  let cnt = 0;
+  visited = [...Array(n)].map(() => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (!visited[i][j] && a[i][j] > d) {
+        dfs(i, j, d);
+        cnt++;
+      }
+    }
+  }
+
+  ret = Math.max(cnt, ret);
+}
+
+console.log(ret);
