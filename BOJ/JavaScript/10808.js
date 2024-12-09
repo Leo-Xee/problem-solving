d97957d7e64578c5b9878536501564de15a6aef3
leo-xee <jang.xmin@gmail.com>
2024-12-10 00:06:39 +0900

    Upload 10808 - 알파벳 개수
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ BOJ/JavaScript/10808.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+const a = Array.from({ length: 26 }).fill(0);
+
+const solution = (input) => {
+  input.split("").forEach((v) => a[v.charCodeAt() - 97]++);
+  return a.join(" ");
+};
+
+console.log(solution(input));
