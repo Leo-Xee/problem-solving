e4ac3177f6bc4df318a121dbbfbf0ad35ea549d1
leo-xee <jang.xmin@gmail.com>
2024-12-11 01:21:31 +0900

    Upload 2979 - 트럭 주차
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ BOJ/JavaScript/2979.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let cnt = 0;
+const t = Array.from({ length: 100 }).fill(0);
+const [a, b, c] = input.shift().split(" ");
+
+const solution = () => {
+  input.forEach((v) => {
+    const [from, to] = v.split(" ").map((v) => +v);
+
+    for (let i = from; i < to; i++) {
+      t[i]++;
+    }
+  });
+
+  t.forEach((v) => {
+    if (v === 1) cnt += v * a;
+    if (v === 2) cnt += v * b;
+    if (v === 3) cnt += v * c;
+  });
+
+  return cnt;
+};
+
+console.log(solution());
