459794a2be65f147160e0315ebd814fe1c9b4fff
LEO <leo@LEO.local>
2024-12-17 01:12:38 +0900

    Upload 10988 - 팰린드롬인지 확인하기
    Signed-off-by: LEO <leo@LEO.local>

---
+++ BOJ/JavaScript/10988.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+const a = input.shift();
+const b = a.split("").reverse().join("");
+
+if (a === b) {
+  console.log(1);
+} else {
+  console.log(0);
+}
