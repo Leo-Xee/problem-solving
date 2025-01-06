2d88996cc08fbff374b3d66b0ee457a174702a59
leo-xee <jang.xmin@gmail.com>
2025-01-06 23:18:53 +0900

    Upload 11655 - ROT13
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ BOJ/JavaScript/11655.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString();
+
+const ret = input.split("").map((v) => {
+  const code = v.charCodeAt();
+
+  if (code >= 65 && code <= 90) {
+    if (code + 13 > 90) {
+      return String.fromCharCode(code - 13);
+    } else {
+      return String.fromCharCode(code + 13);
+    }
+  }
+
+  if (code >= 97 && code <= 122) {
+    if (code + 13 > 122) {
+      return String.fromCharCode(code - 13);
+    } else {
+      return String.fromCharCode(code + 13);
+    }
+  }
+
+  return v;
+});
+
+console.log(ret.join(""));
