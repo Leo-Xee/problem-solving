cd79e014094830a4451fd0316ca62fd581e77e53
leo-xee <jang.xmin@gmail.com>
2025-02-01 17:30:12 +0900

    Upload 1620 - 나는야 포켓몬 마스터 이다솜
    Signed-off-by: leo-xee <jang.xmin@gmail.com>

---
+++ BOJ/JavaScript/1620.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const mp1 = new Map();
+const mp2 = new Map();
+
+const [_, m] = input
+  .shift()
+  .split(" ")
+  .map((v) => +v);
+
+const quizzes = input.splice(-m);
+
+input.forEach((v, idx) => {
+  mp1.set(idx + 1, v);
+  mp2.set(v, idx + 1);
+});
+
+quizzes.forEach((v) => {
+  if (isNaN(Number(v))) {
+    console.log(mp2.get(v));
+  } else {
+    console.log(mp1.get(Number(v)));
+  }
+});
